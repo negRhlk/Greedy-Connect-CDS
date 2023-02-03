459eb3337663688fb57caeed61ac9410dc9113a2
negRhlk <negi.h.10@gmail.com>
2023-02-03 18:40:40 +0330

    The problem of having different component ID is solved

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import copy
 import random
 import networkx as nx
 import numpy as np
 from node import node
 from detect_neighbor import detect_neighbor
-from choice_feature import choice_feature
+# from choice_feature import choice_feature
 import matplotlib.pyplot as plt
 import math
 from pylab import plot, show, savefig, xlim, figure, \
@@ -57,6 +58,16 @@ if random_network == 0:
          40, 99, 101, 107, 65, 95, 47, 70, 110, 43, 83, 63, 65, 89, 93, 115, 56, 88, 115, 120, 113, 94, 58, 76, 119, 76,
          79, 75, 57, 74, 87, 47, 90, 99, 40]
 
+    # x = [16, 22, 2, 3, 12, 7, 19, 1, 23, 17, 4, 7, 21, 3, 20, 13, 15, 16, 5, 8, 6, 22, 5, 11, 18, 20, 6, 2, 5, 1, 5, 4,
+    #      23, 6, 19, 5, 4, 6, 10, 6, 18, 2, 2, 5, 16, 25, 9, 14, 13, 8, 3, 5, 1, 10, 22, 3, 8, 9, 22, 5, 14, 25, 12, 1,
+    #      4, 3, 1, 19, 22, 22, 19, 9, 22, 25, 25, 14, 11, 6, 9, 12, 23, 4, 9, 21, 1, 20, 21, 10, 6, 10, 14, 6, 18, 23, 5,
+    #      12, 15, 12, 9, 2]
+    # y = [7, 24, 21, 18, 14, 24, 17, 20, 5, 20, 12, 12, 15, 12, 1, 1, 20, 8, 15, 3, 25, 8, 12, 5, 9, 15, 23, 1, 24, 11,
+    #      4, 23, 13, 13, 4, 24, 13, 16, 23, 16, 6, 24, 6, 14, 18, 21, 6, 22, 12, 6, 5, 21, 3, 7, 19, 19, 13, 8, 13, 6, 7,
+    #      20, 22, 3, 24, 10, 7, 5, 2, 4, 16, 1, 13, 16, 13, 12, 12, 15, 21, 3, 16, 19, 2, 4, 6, 21, 4, 2, 16, 22, 12, 17,
+    #      3, 22, 19, 5, 10, 21, 7, 16]
+
+
 # Grid topology
 else:
     for i17 in range(int(NUMBER_NODES / math.sqrt(NUMBER_NODES)) + 5):
@@ -83,21 +94,6 @@ for node_source in range(NUMBER_NODES):
 
 # -------------------------- Connected Dominating Set -------------------------
 
-# for nod in nodes:
-#     if nod.COLOR == "yellow":
-#         node_white_count = len(nod.neighbors)  # white_count of the nod
-#         dominator = True
-#
-#         # calculating the white_count of nod's neighbours
-#         for nbr in nod.neighbors:
-#             if len(nodes[nbr].neighbors) > node_white_count:
-#                 dominator = False
-#                 break
-#         if dominator:
-#             nod.COLOR = "black"
-#             for nbr in nod.neighbors:
-#                 nodes[nbr].COLOR = "grey"
-
 # --------- Greedy construction of a dominating set - Phase 1 ------------
 for nod in nodes:
     node_white_count = 0  # white_count of the nod
@@ -122,7 +118,7 @@ for nod in nodes:
                 dominator = False
                 break
         if dominator:
-            nod.COLOR = "black"
+            nod.COLOR = "purple"
             for nbr in nod.neighbors:
                 nodes[nbr].COLOR = "grey"
 
@@ -145,14 +141,14 @@ for nod in nodes:
 
         nbr_white_count[nod.ID] = node_white_count
 
-        temp = 0
-        high = 0
+        temp = -1
+        high = -1
         for key in nbr_white_count:
             if nbr_white_count[key] > temp:
                 temp = nbr_white_count[key]
                 high = key
 
-        nodes[high].COLOR = "black"
+        nodes[high].COLOR = "purple"
         for nbr in nodes[high].neighbors:
             if nodes[nbr].COLOR == "yellow":
                 nodes[nbr].COLOR = "grey"
@@ -166,60 +162,56 @@ for nod in nodes:
         nod.component = -1
         grey_nodes.append(nod)
 
-# Setting black nodes' component ID
-for nod in nodes:
-    if nod.COLOR == "black":
-        node_black_nbr = [-1]
-        for i in nod.neighbors:
-            if nodes[i].COLOR == "black":
-                node_black_nbr.append(nodes[i].ID)
-        if nod.ID > max(node_black_nbr):
-            nod.component = nod.ID
-        else:
-            nod.component = max(node_black_nbr)
-    # print(nod.ID, ':', nod.component)
+    else:
+        nod.component = nod.ID
 
-# for i in range(NUMBER_NODES):
-#     if nodes[NUMBER_NODES - 1 - i].COLOR == "black":
-#         for j in nodes[NUMBER_NODES - 1 - i].neighbors:
-#             if nodes[j].component > nodes[NUMBER_NODES - 1 - i].component:
-#                 nodes[NUMBER_NODES - 1 - i].component = nodes[j].component
-#         print(nodes[NUMBER_NODES - 1 - i].ID, ':', nodes[NUMBER_NODES - 1 - i].component)
+i = NUMBER_NODES - 1
+while True:
+    if i < 0:
+        break
+
+    if nodes[i].COLOR == "purple":
+        node_purple_nbr = [-1]
+        for j in nodes[i].neighbors:
+            if nodes[j].COLOR == "purple":
+                node_purple_nbr.append(nodes[j].component)
+
+        if nodes[i].component > max(node_purple_nbr):
+            for j in nodes[i].neighbors:
+                if nodes[j].COLOR == "purple":
+                    nodes[j].component = nodes[i].component
+        else:
+            nodes[i].component = max(node_purple_nbr)
+
+    i -= 1
 
 # ----------------- Connecting the Dominating Set - Phase 1 -----------------
 for nod in grey_nodes:
     # print(nod.ID)
-    dominators = set()
-    nodes_id = []
+    dominators = set()  # For saving different component IDs
+    # nodes_id = []
     for i in nod.neighbors:
-        if nodes[i].COLOR == "black":
+        if nodes[i].COLOR == "purple":
             dominators.add(nodes[i].component)
-            nodes_id.append(nodes[i].ID)
+            # nodes_id.append(nodes[i].ID)
     dominators = list(dominators)
-    # print(dominators)
 
-    if len(dominators) == 2:
+    if len(dominators) >= 2:
         max_component = max(dominators)
-        nod.COLOR = "black"
+        nod.COLOR = "purple"
         nod.component = max_component
-        for i in nodes_id:
-            if nodes[i].component != max_component:
-                nodes[i].component = max_component
 
-# for i in range(NUMBER_NODES):
-#     print(nodes[i].ID, ':', nodes[i].component)
-
-# for i in range(NUMBER_NODES):
-#     if nodes[NUMBER_NODES - 1 - i].COLOR == "black":
-#         for j in nodes[NUMBER_NODES - 1 - i].neighbors:
-#             if nodes[j].component > nodes[NUMBER_NODES - 1 - i].component:
-#                 nodes[NUMBER_NODES - 1 - i].component = nodes[j].component
-#         print(nodes[NUMBER_NODES - 1 - i].ID, ':', nodes[NUMBER_NODES - 1 - i].component)
+        # Propagating component ID
+        for j in nodes:
+            if j.component in dominators:
+                j.component = max_component
 
 # ----------------- Connecting the Dominating Set - Phase 2 -----------------
 for u in nodes:
+    # If U is a grey node
     if u.COLOR == "grey":
         for i in u.neighbors:
+            # If U has a grey neighbor V
             if nodes[i].COLOR == "grey":
                 highU = -1
                 highV = -1
@@ -237,32 +229,32 @@ for u in nodes:
                     # print('v: ', nodes[i].ID, 'highV: ', highV)
                     # print("----------------------")
 
-                    u.COLOR = "black"
-                    nodes[i].COLOR = "black"
+                    u.COLOR = "purple"
+                    nodes[i].COLOR = "purple"
 
                     u.component = max([highU, highV])
                     nodes[i].component = max([highU, highV])
+
+                    # Propagating component ID
+                    for k in nodes:
+                        if k.component == min([highU, highV]):
+                            k.component = max([highU, highV])
                 break
             break
 
-# for i in range(NUMBER_NODES):
-    # if nodes[NUMBER_NODES - 1 - i].COLOR == "black":
-    #     for j in nodes[NUMBER_NODES - 1 - i].neighbors:
-    #         if nodes[j].component > nodes[NUMBER_NODES - 1 - i].component:
-    #             nodes[NUMBER_NODES - 1 - i].component = nodes[j].component
-    #         else:
-    #             nodes[j].component = nodes[NUMBER_NODES - 1 - i].component
-    # print(nodes[NUMBER_NODES - 1 - i].ID, ':', nodes[NUMBER_NODES - 1 - i].component)
+for i in range(NUMBER_NODES):
+    print(nodes[i].ID, ':', nodes[i].component)
 
 # -------------------------- Printing Dominator Nodes ------------------------    
 for i in range(NUMBER_NODES):
-    if nodes[i].COLOR == "black":
+    if nodes[i].COLOR == "purple":
         Relay_Node.append(nodes[i].ID)
 
 print(Relay_Node)
+print(len(Relay_Node))
 # --------------------------- Plot Network Topology ---------------------------
 # print(nx.info(Gar))
-fig = figure()  # it is possible to use figsize=(num, num)
+fig = figure(figsize=(7, 7))  # it is possible to use figsize=(num, num)
 color = [nodes[i].COLOR for i in range(NUMBER_NODES)]
 
 pos = {}
@@ -270,5 +262,7 @@ for i in range(len(Gar.nodes)):
     pos[i] = np.array([nodes[i].Xposition, nodes[i].Yposition])
 
 nx.draw(Gar, pos=pos, with_labels=True, node_color=color)
+# nx.draw(Gar, with_labels=True, node_color=color)
 plt.savefig('topology.png', dpi=200, bbox_inches='tight')
 
+print(Gar.nodes)
